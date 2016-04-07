3ec0c29bf581e7fb3713310965d812846c222451
Julio Vasquez <julio.vasquez@adesis.com>
2016-04-07 10:37:07 -0500

    feat(): agrega las tareas básicas para el sitio

--- gulpfile.js
+++ gulpfile.js
@@ -2,42 +2,55 @@ var gulp = require('gulp');
 var gulpsync = require('gulp-sync')(gulp);
 var clean = require('gulp-clean');
 var jade = require('gulp-jade');
-var livereload = require('gulp-livereload');
+//var livereload = require('gulp-livereload');
 var minifyCSS = require('gulp-minify-css');
+var server = require('gulp-server-livereload');
 var stylus = require('gulp-stylus');
 var watch = require('gulp-watch');
 
 // Task to RUN the proyect
-gulp.task('run', ['css', 'jade', 'watch']);
+gulp.task('run', ['css', 'jade', 'watch', 'webserver']);
 
 
 // Task clean folder Dist
 gulp.task('clean-dist', function () {
-  return gulp.src('./dist', {read: false})
-    .pipe(clean());
+	return gulp.src('./dist', {read: true})
+	.pipe(clean());
 });
 
 // Task make Jade to HTML
 gulp.task('jade', ['clean-dist'], function() {
-   gulp.src('./app/view/**/*.jade')
-   .pipe(jade({
-      pretty: true
-   }))
-   .pipe(gulp.dest('./dist'))
-   .pipe(livereload());
+	gulp.src('./app/view/**/*.jade')
+	.pipe(jade({
+		pretty: true
+	}))
+	.pipe(gulp.dest('./dist'))
+	//.pipe(livereload());
 });
 
 // Task make Stylus to CSS
 gulp.task('css', ['clean-dist'], function() {
-   gulp.src('./app/styles/**/*.styl')
-   .pipe(stylus())
-   //.pipe(minifyCSS())
-   .pipe(gulp.dest('./dist/css/'))
-   .pipe(livereload());
+	gulp.src('./app/styles/**/*.styl')
+	.pipe(stylus())
+	//.pipe(minifyCSS())
+	.pipe(gulp.dest('./dist/css/'))
+	//.pipe(livereload());
 });
 
 // Task for watch the folders Styles and View
-gulp.task('watch', function() {
-   livereload.listen();
-   gulp.watch(['./app/styles/**/*.styl', './app/view/**/*.jade'], ['css', 'jade'])
+gulp.task('watch', ['clean-dist'], function() {
+	//livereload.listen();
+	gulp.watch(
+		['./app/styles/**/*.styl', './app/view/**/*.jade'], 
+		['css', 'jade']
+	)
+});
+
+gulp.task('webserver', function() {
+	gulp.src('dist')
+		.pipe(server({
+			livereload: true,
+			defaultFile: 'index.html',
+			open: true
+		}));
 });
